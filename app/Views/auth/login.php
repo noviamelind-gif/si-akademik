ae80e5cb5364040e39ccd73c8c0467f6c447074c
noviamelinda <noviamelind@gmail.com>
2026-09-12 15:07:23 +0700

    mengubah judul login pada main

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -19,7 +19,7 @@ session_start();
                 <div class="card-body">
                     <h2 class="text-center mb-4">
                         Login SI Akademik
-                        Silakan login untuk mengakses sistem akademik.
+                        Silakan login untuk mengakses si akademik!
                     </h2>
                         <?php 
                             if (isset($_SESSION['flash_message'])): 
